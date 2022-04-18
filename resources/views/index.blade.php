fe5c1a22863e39791f0086412ef336a6ffc5b107
nathaly.mag <nathaly.cassia@prefeitura.sp.gov.br>
2022-04-18 17:12:06 -0300

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,30 @@
+@include('layout')
+
+<div class="row justify-content-center" style="width: 70rem; margin-left: 160px">
+    <div class="col">
+      <div class="card mt-4">
+        <img src="{{ asset('img/gato.jpeg')}}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Cuidados com o seu pet</h5>
+          <p class="card-text">Uma equipe especializada garatindo os melhores cuidados.</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="col">
+      <div class="card mt-4">
+        <img src="{{ asset('img/bed.jpg')}}" class="card-img-top">
+        <div class="card-body">
+          <h5 class="card-title">Produtos feitos pra eles</h5>
+          <p class="card-text">Invista no entrenimento animal, eles também merecem!</p>
+        </div>
+      </div>
+    </div>
+</div>
+
+<div class="card mt-3" style="width: 18rem; margin-left: 560px">
+    <div class="card-body text-center px-4">
+        <h5 class="card-title">Garanta já o seu</h5>
+        <a href="{{ url('/cadastro')}}" class="btn btn-outline-primary">Cadastre-se</a>
+    </div>
+</div>
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Petshop</title>
+    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon-16x16.png">
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light" style="background-color: #d4bbe8">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#" style="color: #fff; font-size: 30px; font-weight: bold"><img src="{{ asset('img/logo.png')}}" width="40"> Petshop</a>
+          <div class="collapse navbar-collapse d-lg-flex justify-content-end" id="navbarNav">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="#"><button type="button" class="btn btn-outline-light">
+                    Produtos </button></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ url('/cadastro') }}"><button type="button" class="btn btn-outline-light">
+                    Cadastro </button></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#"><button type="button" class="btn btn-outline-light">
+                    Contato </button></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+</body>
+</html>
