aa41043ba4e7edba1e75ac8bc99cca7d6e483e5b
nathaly.mag <nathaly.cassia@prefeitura.sp.gov.br>
2022-05-02 16:30:06 -0300

    a#href

--- resources/views/produto/toy.blade.php
+++ resources/views/produto/toy.blade.php
@@ -4,7 +4,7 @@
 <h3 style="position: absolute; top: 100px; right: 60%">Brinquedos</h3>
 
 <div class="d-flex" style="position: fixed; top: 170px; left: 350px">
-<i class="bi bi-basket2 fs-2 mx-auto px-3"></i>
+<a href="#"><i class="bi bi-basket2 fs-2 mx-auto px-3"></i></a>
 <div class="card align-items-center" style="width: 15rem;">
     <img src="{{ asset('img/brinq.jpg')}}" width="130px">
         <h5 class="card-title">Ossinho</h5>
